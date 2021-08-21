1407fb4fecfd1701d6d4b4d25e79a5ff01f34ff2
Sandradjuricin9 <sandradjuricin9@gmail.com>
2021-08-22 01:17:59 +0200

    dodati novi testovi

--- SandraDjuricin/DizajnerskiObrasci/test/com/sekara/designpatterns/command/CmdToBackTest.java
+++ SandraDjuricin/DizajnerskiObrasci/test/com/sekara/designpatterns/command/CmdToBackTest.java
@@ -2,18 +2,47 @@ package com.sekara.designpatterns.command;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.awt.Color;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import com.sekara.designpatterns.model.ModelDrawing;
+import com.sekara.designpatterns.model.geometry.Point;
 
 class CmdToBackTest {
+	private Point firstPoint;
+	private Point secondPoint;
+	private ModelDrawing model;
+	private int indexOfShape;
+	private CmdToBack cmdToBack;
+	
+	@BeforeEach
+	public void initialization() {
+		model = new ModelDrawing();
+		firstPoint = new Point(15,20, Color.BLACK);
+		secondPoint = new Point(5,10, Color.BLACK);
+   		model.addShape(firstPoint);
+		model.addShape(secondPoint);
+		indexOfShape = model.getIndexOfShape(secondPoint);
 
+		cmdToBack = new CmdToBack(secondPoint, model);
+ 	}
+	
 	@Test
-	void testExecute() {
-		fail("Not yet implemented");
+	public void testUnExecuteExecuteNotCalled() {
+		cmdToBack.unExecute();
+		assertEquals(indexOfShape,model.getIndexOfShape(secondPoint));
+	}
+	
+	@Test
+	public void testExecute() {
+		cmdToBack.execute();
+		assertEquals(indexOfShape-1,model.getIndexOfShape(secondPoint));
 	}
 
 	@Test
-	void testUnExecute() {
-		fail("Not yet implemented");
+	void testUnExecuteAfterExecute() {
+		cmdToBack.unExecute();
+		assertEquals(indexOfShape,model.getIndexOfShape(secondPoint));
 	}
-
 }
